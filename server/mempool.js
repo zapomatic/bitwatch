f559771c8393e62d925830702cb45c48436a5459
Zap-O-Matic <zapomatic@proton.me>
2025-04-06 12:17:49 -0700

    auto-accept and savedb when changes occur in websocket

--- server/mempool.js
+++ server/mempool.js
@@ -122,6 +122,7 @@ const calculateAddressBalance = (transactions, address) => {
 };
 
 const updateAddressBalance = (address, balance, io) => {
+  let changesDetected = false;
   for (const [collectionName, collection] of Object.entries(
     memory.db.collections
   )) {
@@ -137,37 +138,51 @@ const updateAddressBalance = (address, balance, io) => {
         return addr.monitor[type] === "alert";
       };
 
-      if (
-        newBalance.chain_in !== addr.expect.chain_in &&
-        needsAlert("chain_in")
-      ) {
+      // Always log changes, regardless of monitor setting
+      if (newBalance.chain_in !== addr.expect.chain_in) {
         changes.chain_in = newBalance.chain_in;
+        if (!needsAlert("chain_in")) {
+          logger.info(
+            `auto-accept chain_in (${newBalance.chain_in}) for ${address} (${collectionName}/${addr.name})`
+          );
+          addr.expect.chain_in = newBalance.chain_in;
+        }
       }
-      if (
-        newBalance.chain_out !== addr.expect.chain_out &&
-        needsAlert("chain_out")
-      ) {
+      if (newBalance.chain_out !== addr.expect.chain_out) {
         changes.chain_out = newBalance.chain_out;
+        if (!needsAlert("chain_out")) {
+          logger.info(
+            `auto-accept chain_out (${newBalance.chain_out}) for ${address} (${collectionName}/${addr.name})`
+          );
+          addr.expect.chain_out = newBalance.chain_out;
+        }
       }
-      if (
-        newBalance.mempool_in !== addr.expect.mempool_in &&
-        needsAlert("mempool_in")
-      ) {
+      if (newBalance.mempool_in !== addr.expect.mempool_in) {
         changes.mempool_in = newBalance.mempool_in;
+        if (!needsAlert("mempool_in")) {
+          logger.info(
+            `auto-accept mempool_in (${newBalance.mempool_in}) for ${address} (${collectionName}/${addr.name})`
+          );
+          addr.expect.mempool_in = newBalance.mempool_in;
+        }
       }
-      if (
-        newBalance.mempool_out !== addr.expect.mempool_out &&
-        needsAlert("mempool_out")
-      ) {
+      if (newBalance.mempool_out !== addr.expect.mempool_out) {
         changes.mempool_out = newBalance.mempool_out;
+        if (!needsAlert("mempool_out")) {
+          logger.info(
+            `auto-accept mempool_out (${newBalance.mempool_out}) for ${address} (${collectionName}/${addr.name})`
+          );
+          addr.expect.mempool_out = newBalance.mempool_out;
+        }
       }
 
       // Update the address with new balance
       addr.actual = newBalance;
 
-      // If there are changes that need alerts, log them
+      // If there are any changes, log them and mark for save
       if (Object.keys(changes).length > 0) {
-        logger.warning(`Balance mismatch detected for ${address} (${collectionName}/${addr.name}):
+        changesDetected = true;
+        logger.info(`Balance changes detected for ${address} (${collectionName}/${addr.name}):
 Expected: chain_in=${addr.expect.chain_in}, chain_out=${addr.expect.chain_out}, mempool_in=${addr.expect.mempool_in}, mempool_out=${addr.expect.mempool_out}
 Actual: chain_in=${newBalance.chain_in}, chain_out=${newBalance.chain_out}, mempool_in=${newBalance.mempool_in}, mempool_out=${newBalance.mempool_out}
 Previous: chain_in=${oldBalance.chain_in}, chain_out=${oldBalance.chain_out}, mempool_in=${oldBalance.mempool_in}, mempool_out=${oldBalance.mempool_out}`);
@@ -177,6 +192,11 @@ Previous: chain_in=${oldBalance.chain_in}, chain_out=${oldBalance.chain_out}, me
       io.emit("updateState", { collections: memory.db.collections });
       return true;
     }
+  }
+
+  // If any changes were detected, save the database
+  if (changesDetected) {
+    memory.saveDb();
   }
   return false;
 };
