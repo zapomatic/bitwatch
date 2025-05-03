b9916d298cbcf16b868460006e3310942c3110c9
Zap-O-Matic <zapomatic@proton.me>
2025-05-02 20:41:00 -0700

    remove old test

--- client/src/__tests__/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "../App";
-
-describe("App", () => {
-  it("renders without crashing", () => {
-    render(<App />);
-    expect(screen.getByText("itwatch")).toBeInTheDocument();
-  });
-});
